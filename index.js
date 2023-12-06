25f2ba4f2bad4bef8721670ceffe11798ec558da
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2023-12-06 09:56:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function logger(req, res, next) {
 }
 
 app.use(logger);
+const PORT = 3000;
+ 
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
