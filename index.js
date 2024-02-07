3b9b26b7cc4d6c0bcd7a78e59dfb9f60207e9066
chessaimaker <147644008+chessaimaker@users.noreply.github.com>
2024-02-06 19:07:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { request } = require("request");
-const { express } = require("express");
-const { path } = require("path");
-const { fs } = require("fs");
-const { session }= require("express-session");
+const request = require("request");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const session = require("express-session");
 const { Transform } = require("stream");
 const app = express();
 
